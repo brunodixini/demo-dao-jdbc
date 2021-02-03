f67431acc5e5cbaf3f960c3e1bcbdf60f9564772
brunodixini <brunodixini@gmail.com>
2021-02-03 15:14:04 -0300

    DepartmentDaoJDBC and findById, findAll, insert, update, deleteById methods

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,11 +1,18 @@
 package model.dao;
 
 import db.DB;
+import model.dao.impl.DepartmentDaoJDBC;
 import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
 	
+	// this class exposes externally a SellerDao Interface, but internally instantiate an implementation
+	// we did this to don't expose the implementation
 	public static SellerDao createSellerDao() {
 		return new SellerDaoJDBC(DB.getConnection());
+	}
+	
+	public static DepartmentDao createDepartmentDao() {
+		return new DepartmentDaoJDBC(DB.getConnection());
 	}
 }
